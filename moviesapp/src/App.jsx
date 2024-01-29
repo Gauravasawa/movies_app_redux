830855f46f03b48f6510d8ecdae75d21e8c3e229
Gauravasawa <asawagaurav3@gmail.com>
2024-01-29 19:15:04 +0530

    added routing

--- moviesapp/src/App.jsx
+++ moviesapp/src/App.jsx
@@ -1,10 +1,16 @@
 import { useEffect } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css'
 import { fetchDataApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from './store/homeSlice'
-
-
+import Header from './componets/header/Header'
+import Footer from './componets/footer/Footer'
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import SearchResult from './pages/searchResult/SearchResult'
+import Explore from './pages/explore/Explore'
+import PageNotFound from './pages/404/PageNotFound'
 
 function App() {
 
@@ -26,9 +32,17 @@ function App() {
   }
 
   return (
-    <div className='App'>
-      App{url?.total_pages}
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:mediaType/:id' element={<Details />} />
+      <Route path='/search/:query' element={<SearchResult />} />
+      <Route path='/explore/:mediaType' element={<Explore />}/>
+      <Route path='*' element={<PageNotFound />}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   )
 }
 
