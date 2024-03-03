67eec4b14531384b5f2fc6f175c3f59c62dd9bd3
Gauravasawa <asawagaurav3@gmail.com>
2024-03-03 11:22:10 +0530

    added cast, deatils Banner play videos, video popup & cast details

--- moviesapp/src/pages/details/Details.jsx
+++ moviesapp/src/pages/details/Details.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import "./style.scss";
+
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
-export default Details
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.Cast} loading={creditsLoading} />
+    </div>
+  );
+};
+
+export default Details;
